9e17b3d0fb6aeb865a0cf04683618ff6ea59280c
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-06-22 04:26:35 +0300

    feat: server setup complete

--- server/controllers/article.ctrl.js
+++ server/controllers/article.ctrl.js
@@ -37,6 +37,14 @@ module.exports = {
   getAll: (req,res,next) => {
     Article.find(req.params.id)
     .populate('author')
-    .populate('comments.author')
+    .populate('comments.author').exec((err, article)=> {
+      if(err)
+      res.send(err)
+      else if(!article)
+      res.send(404)
+      else
+      res.send(article)
+      next()
+    })
   }
 }
